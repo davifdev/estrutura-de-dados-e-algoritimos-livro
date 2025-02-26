7e52adbc14e341bed82a792568c4deae8a600dc4
Davi Fernandes <davi.fer159@gmail.com>
2025-02-26 17:38:08 -0300

    function :sparkles:

---
+++ cap-01/07-Functions.js
@@ -0,0 +1,20 @@
+// Sintaxe Básica
+function sayHello() {
+  console.log("Hello!");
+}
+
+sayHello();
+
+// Funções com argumentos
+function output(text) {
+  console.log(text);
+}
+
+output("Hello");
+
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+var result = sum(4,6);
+console.log(result);
